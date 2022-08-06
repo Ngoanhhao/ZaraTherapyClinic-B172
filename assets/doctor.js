bc150593ef24a61da76606d2c5c2cf261016f3be
Hao Ngo <ngoanhhao24@gmail.com>
2022-08-06 11:13:58 +0700

    update new index

---
+++ assets/doctor.js
@@ -0,0 +1,57 @@
+var doctors = [
+    {
+        img:"assets/Img/doctors/bac-si-wade.png",
+        info:`"Dr Wade Brackenbury was born in Idaho, in the U.S.A. At the age of 16, he fractured his
+        lumbar spine playing American football. After three years of crippling pain, which motivated him to seek
+        treatment from a number of different health care practitioners, he was successfully treated by a German
+        doctor who practiced Chiropractic and acupuncture together. Doctor Brackenbury considers his injury and
+        recovery the most formative event in his life, and it sparked an intense life long interest in the
+        treatment of pain and chronic disability."`
+    },
+    {
+        img:"assets/Img/doctors/bac-si-hoisang-goong.png",
+        info:`"Dr. Hoisang was born in Seoul, South Korea and grew up in the United States.
+
+        He understands and appreciates the value of being able to bring the most effective Western treatment of
+        pain to the Asian community.
+
+        Taking care of his patients’ health is Dr. Hoisang’s greatest mission and he strives for continuous
+        enhancements to ensure that his patients receive the best treatment."`
+    },
+    {
+        img:"assets/Img/doctors/bac-si-eric.png",
+        info:`"Dr. Eric Balderree believes it is very important to take time and explain to patients about
+        their diagnosis in order for them to fully understand what factors lead to their current state of health.
+        With an individualized treatment plan specific to each patient, he oversees his patients’ progress in pain
+        management, as well as preventative care."`
+    },
+    {
+        img:"assets/Img/doctors/marc-bio.png",
+        info:`Born and raised in New York, Dr. Marc Tafuro knew he wanted to be a Doctor of Chiropractic at a young age. 
+        Active in sports in high school he knew he not only wanted to help people who were in pain or disabled but also wanted to help athletes reach peak performance.   `
+    },
+    {
+        img:"assets/Img/doctors/bac-si-luke-hamman.png",
+        info:`Dr. Luke Hamman was born in Michigan and has been exposed to Chiropractic in his entire childhood as his best friend’s father was a Chiropractor.
+
+        He benefited much from the regular checkups as well as adjustments to keep his spine and body healthy. His friend’s father also helped guide and mentor Dr. Luke as he went through his studies in becoming a Chiropractor himself.`
+    },
+    {
+        img:"assets/Img/doctors/bac-si-rabie.png",
+        info:`"Dr. Rob Sleiman began to develop severe back problems during his postgraduate studies.
+        After consulting a number of medical doctors and taking several muscle relaxants as well as painkillers,
+        he finally consulted a chiropractor. In just a few visits, Dr. Rob found his condition much improved. This
+        opened up a new perspective for Dr. Rob with further inspiration for him to look more into this
+        profession."`
+    },
+    {
+        img:"assets/Img/doctors/bac-si-aubrey-c-gail.png",
+        info:`Dr. Aubrey C. Gail was born in Greensboro North Carolina and has been a practicing chiropractor for well over 22 years. He chose this field as it is one of the world’s largest natural healing professions, which seeks to identify the root cause of patient’s problem.  He combined his knowledge of chiropractic with nutrition and exercise in order to create a total health experience for the patient.`
+    },
+    {
+        img:"assets/Img/doctors/bac-si-tim.png",
+        info:`"For over twenty years, Dr. Tim has been living and practicing Chiropractic as the critical
+        part of his life. After a sports injury in his early 20’s, he was introduced to Chiropractic and realized
+        his true passion in learning and sharing ideas on natural healing methods with others."`
+    }
+]
